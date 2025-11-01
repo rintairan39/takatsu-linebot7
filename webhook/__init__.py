5f894b8388c90e43036cba4727c4c8c2149e73ba
rintairan39 <rintairan39@gmail.com>
2025-11-01 14:45:35 +0900

    Create __init__.py

---
+++ webhook/__init__.py
@@ -0,0 +1,10 @@
+import sys, os
+# ここを追加： wwwroot/.python_packages/lib/site-packages をパスに通す
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".python_packages", "lib", "site-packages"))
+
+import azure.functions as func
+import googleapiclient.discovery  # ここで失敗しないはず
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse("reloaded: 0 docs", status_code=200)
+  
